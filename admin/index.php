76644127d04a37f8dd945605335cce5e7b82f282
yaelahnong <45358825+yaelahnong@users.noreply.github.com>
2020-04-29 22:47:57 +0700

    Menambahkan kondisi pada request API

--- admin/index.php
+++ admin/index.php
@@ -1,14 +1,16 @@
 <?php 
   session_start();
   if(!isset($_SESSION["login"])) {
-    header("Location: login.php");
+    header("Location: ../login.php");
+  } else {
+
+    
+    $api_corona = file_get_contents('https://api.kawalcorona.com/indonesia');
+    
+    $data = json_decode($api_corona, true);
+    
+    $data = $data[0];
   }
-
-  $api_corona = file_get_contents('https://api.kawalcorona.com/indonesia');
-
-  $data = json_decode($api_corona, true);
-
-  $data = $data[0];
 
 
 ?>
